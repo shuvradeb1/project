368cae386d36acd3c3bd63370386ed430758d54c
shuvradeb1 <shuvradeb67@gmail.com>
2023-10-14 12:17:56 +0600

    project commit 1

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import "../Styles/App.css";
+import Layout from "./Layout";
+// import Home from "./pages/Home";
+import Signup from "./pages/Signup";
+
+function App() {
+  return (
+   <Layout>
+    {/* <Home /> */}
+    <Signup/>
+   </Layout>
+  );
+}
+
+export default App;
